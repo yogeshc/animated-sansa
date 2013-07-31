c87bbd7fda254d19ecd6b25cc5299aab9fb95ddf
Yogesh Chaudhari <mr.yogesh@gmail.com>
2013-08-01 01:42:56 +0530

    Created models for Items in the list

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -60,3 +61,34 @@ class HomePageTest(TestCase):
             'home.html', {'new_item_text': 'A new list item'}
         )
         self.assertEqual(response.content, expected_html)
+
+
+class ItemModelTest(TestCase):
+    """
+    You can see that creating a new record in the database is a relatively
+    simple matter of creating an object, assigning some attributes, and
+    calling a .save() function. Django also gives us an API for querying the
+    database via a class method, .objects, and we use the simplest possible
+    query, .all(), which retrieves all the records for that table. The
+    results are returned as a list-like object called a QuerySet, which
+    we can call further functions on, like .count(), and also extract
+    individual objects. We then check the objects as saved to the database,
+    to check whether the right information was saved
+    """
+
+    def test_saving_and_retriving_items(self):
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second list item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items(0)
+        second_saved_item = saved_items(1)
+        self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(second_saved_item.text, 'The second list item')
